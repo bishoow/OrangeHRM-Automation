72ae19542a669723b183bcf0dbec84fa183baff9
Bishow deep <shresthabishow11@gmail.com>
2025-11-19 22:00:23 +0545

    Implemented login feature

--- cypress/POMpages/loginpage.js
+++ cypress/POMpages/loginpage.js
@@ -1,11 +1,30 @@
-export class LoginPage{
-    userNamelocator='[placeholder="Username"]'
-    passwordlocator='[placeholder="Password"]'
-    loginlocator="button[type='submit']"
-    
-    Ulogin(username,password){
-        cy.get(this.userNamelocator).type(username).should('have.value',username)
-        cy.get(this.passwordlocator).clear().type(password).should('have.value',password)
-        cy.get(this.loginlocator).click()
+// cypress/POMpages/loginpage.js
+
+export class LoginPage {
+  userNamelocator = '[placeholder="Username"]'
+  passwordlocator = '[placeholder="Password"]'
+  loginlocator = "button[type='submit']"
+
+  Ulogin(username, password) {
+    cy.get(this.userNamelocator).clear().type(username).should('have.value', username)
+    cy.get(this.passwordlocator).clear().type(password).should('have.value', password)
+    cy.get(this.loginlocator).click()
+  }
+
+  Ulogininvalid(username, password) {
+    cy.get(this.userNamelocator).clear()
+    if (username) {
+      cy.get(this.userNamelocator).type(username).should('have.value', username)
     }
+    cy.get(this.passwordlocator).clear()
+    if (password) {
+      cy.get(this.passwordlocator).type(password).should('have.value', password)
+    }
+    cy.get(this.loginlocator).click()
+  }
+
+  // loginWithoutUsername(password) {
+  //   cy.get(this.passwordlocator).clear().type(password).should('have.value', password)
+  //   cy.get(this.loginlocator).click()
+  // }
 }
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,6 +3,13 @@ describe('Login Test', () => {
     cy.login()
     // cy.url().should('include', '/inventory.html')
   })
+   it('should fail to login with invalid credentials', () => {
+    cy.logininvalid('invalidUser', 'invalidPass')
+  })
+
+  it('should fail to login without username', () => {
+    cy.loginwithoutUsername()
+  })
   it('should forgot successfully', () => {
     cy.forgot()
     // cy.url().should('include', '/inventory.html')
